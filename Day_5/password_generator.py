577c749acac06450261f13b87c64def0381d570a
patterson-dtaylor <patterson.dtaylor@gmail.com>
2021-07-20 07:36:50 -0400

    added day 5

---
+++ Day_5/password_generator.py
@@ -0,0 +1,19 @@
+import random
+
+# Global Variables
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+characters = [letters, numbers, symbols]
+
+password = []
+
+password_length = int(input("How many characters do you want your password?\n We suggest at least 12. "))
+
+for i in range(0, password_length):
+    index = random.randint(0, 2)
+    field = characters[index]
+    password.append(random.choice(field))
+ 
+print(f"Your password is: {''.join(password)}")
